3178b3a80f21085cd285dfb70e43c8fed06c0fcf
irizhukova <ira.zhu.2003@gmail.com>
2025-03-17 23:08:58 -0400

    implement dotenv file for security

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhLjuOSN7HcPw5Nn-NPRottw7k4mmGMwQ",
-  authDomain: "meeting-manager-e2601.firebaseapp.com",
-  projectId: "meeting-manager-e2601",
-  storageBucket: "meeting-manager-e2601.firebasestorage.app",
-  messagingSenderId: "197906993909",
-  appId: "1:197906993909:web:383af7e8a5a88205ce2bbc",
-  measurementId: "G-EHBFE18M5J"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
